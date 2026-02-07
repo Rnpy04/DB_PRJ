2e0de2d8bbd4a99580510576cbdf8e3ec30960c5
Rnpy04 <arenyusefi@gmail.com>
2026-02-07 06:25:22 +0330

    reorder views

--- prj2/phase4-index.sql
+++ prj2/phase4-index.sql
@@ -1,22 +1,22 @@
--- DROP INDEX IF EXISTS idx_transaction_date;
--- DROP INDEX IF EXISTS idx_branch_city_street;
--- DROP INDEX IF EXISTS idx_account_balance;
--- DROP INDEX IF EXISTS idx_active_accounts_balance;
+DROP INDEX IF EXISTS idx_transaction_date;
+DROP INDEX IF EXISTS idx_branch_city_street;
+DROP INDEX IF EXISTS idx_account_balance;
+DROP INDEX IF EXISTS idx_active_accounts_balance;
 
 CREATE INDEX idx_transaction_date ON transactions(transaction_date DESC);
 
-EXPLAIN ANALYZE
-SELECT * FROM transactions 
-WHERE transaction_date >= CURRENT_DATE - INTERVAL '7 days'
-ORDER BY transaction_date DESC;
+-- EXPLAIN ANALYZE
+-- SELECT * FROM transactions 
+-- WHERE transaction_date >= CURRENT_DATE - INTERVAL '7 days'
+-- ORDER BY transaction_date DESC;
 
 --================================================================
 CREATE INDEX idx_branch_city_street ON branch(city,street);
 
-EXPLAIN ANALYZE
-SELECT b.*
-FROM branch b
-WHERE b.city = 'تهران' and b.street = 'خیابان ولیعصر';
+-- EXPLAIN ANALYZE
+-- SELECT b.*
+-- FROM branch b
+-- WHERE b.city = 'تهران' and b.street = 'خیابان ولیعصر';
 
 --================================================================
 --هزینه زیاد
@@ -30,19 +30,19 @@ WHERE b.city = 'تهران' and b.street = 'خیابان ولیعصر';
 --================================================================
 CREATE INDEX idx_account_balance ON account(balance);
 
-EXPLAIN ANALYZE
-SELECT a.*
-FROM account a
-WHERE a.balance >200000;
+-- EXPLAIN ANALYZE
+-- SELECT a.*
+-- FROM account a
+-- WHERE a.balance >200000;
 --================================================================
 CREATE INDEX idx_active_accounts_balance ON account(balance DESC) 
 WHERE status = 'ACTIVE';
 
-EXPLAIN ANALYZE
-SELECT * FROM account 
-WHERE status = 'ACTIVE' 
-  AND balance > 10000000
-ORDER BY balance DESC;
+-- EXPLAIN ANALYZE
+-- SELECT * FROM account 
+-- WHERE status = 'ACTIVE' 
+--   AND balance > 10000000
+-- ORDER BY balance DESC;
 -- --================================================================
 -- --این کلید یونیکه  ولی گفتم شاید استفاده شه
 -- CREATE INDEX idx_customer_national_id ON customer(national_id);
