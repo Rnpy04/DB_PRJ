9274b0c657779281e22094c20301a79bc72614ee
Rnpy04 <arenyusefi@gmail.com>
2026-02-07 07:41:30 +0330

    cleaning

--- prj2/phase9_audit.sql
+++ prj2/phase9_audit.sql
@@ -9,6 +9,8 @@ DROP INDEX IF EXISTS idx_audit_changed_by;
 DROP INDEX IF EXISTS idx_audit_operation;
 DROP TABLE IF EXISTS audit_log CASCADE;
 
+---------------------------------------------------------------------------------------------------
+
 CREATE TABLE audit_log (
     audit_id SERIAL PRIMARY KEY,
     table_name VARCHAR(50) NOT NULL,
@@ -21,20 +23,25 @@ CREATE TABLE audit_log (
     CONSTRAINT chk_operation CHECK (operation_type IN ('INSERT', 'UPDATE', 'DELETE'))
 );
 
+--خودم ایندکس گذاشتم شاید به کار بیاد
 CREATE INDEX idx_audit_table_name ON audit_log(table_name);
 CREATE INDEX idx_audit_changed_at ON audit_log(changed_at DESC);
 CREATE INDEX idx_audit_changed_by ON audit_log(changed_by);
 CREATE INDEX idx_audit_operation ON audit_log(operation_type);
 
+---------------------------------------------------------------------------------------------------
+
 --فانکشن برای ثبت
+--تابع عمومی برای همه
 CREATE OR REPLACE FUNCTION fn_audit_trigger()
 RETURNS TRIGGER
 LANGUAGE plpgsql
 AS $$
 DECLARE
-    v_old_data JSONB;
+    v_old_data JSONB;--برای انعطاف بیشتر با این نوع داده
     v_new_data JSONB;
 BEGIN
+    --تشخیص نوع عملیات
     IF (TG_OP = 'DELETE') THEN
         v_old_data := row_to_json(OLD)::JSONB;
         v_new_data := NULL;
@@ -53,7 +60,9 @@ BEGIN
         old_values,
         new_values,
         changed_by,
-        changed_at
+        changed_at,
+        client_ip,        
+        application_name  
     )
     VALUES (
         TG_TABLE_NAME,
@@ -65,7 +74,9 @@ BEGIN
         v_old_data,
         v_new_data,
         CURRENT_USER,
-        CURRENT_TIMESTAMP
+        CURRENT_TIMESTAMP,
+        inet_client_addr(),           
+        current_setting('application_name')
     );
 
     IF (TG_OP = 'DELETE') THEN
@@ -81,10 +92,12 @@ CREATE TRIGGER trg_audit_account
 AFTER INSERT OR UPDATE OR DELETE ON account
 FOR EACH ROW EXECUTE FUNCTION fn_audit_trigger();
 
+--برای امنیت بیشتر ..با اینکه خود تراکنش نوعی لاگ است ولی یکی ممکنه پاک کنه تراکنش انجام شده رو
 CREATE TRIGGER trg_audit_transaction
 AFTER INSERT OR UPDATE OR DELETE ON transactions
 FOR EACH ROW EXECUTE FUNCTION fn_audit_trigger();
 
+---------------------------------------------------------------------------------------------------
 
 --ویو ها
 
@@ -119,48 +132,52 @@ FROM audit_log
 GROUP BY table_name, operation_type
 ORDER BY table_name, operation_type;
 
+---------------------------------------------------------------------------------------------------
+
 --تست ها
 -- تغییر موجودی حساب
-DO $$
-DECLARE
-    v_audit_count_before INTEGER;
-    v_audit_count_after INTEGER;
-BEGIN
-    SELECT COUNT(*) INTO v_audit_count_before FROM audit_log WHERE table_name = 'account';
+-- DO $$
+-- DECLARE
+--     v_audit_count_before INTEGER;
+--     v_audit_count_after INTEGER;
+-- BEGIN
+--     SELECT COUNT(*) INTO v_audit_count_before FROM audit_log WHERE table_name = 'account';
     
-    UPDATE account 
-    SET balance = balance + 100000 
-    WHERE account_number = '1001000100010001';
+--     UPDATE account 
+--     SET balance = balance + 100000 
+--     WHERE account_number = '1001000100010001';
     
-    SELECT COUNT(*) INTO v_audit_count_after FROM audit_log WHERE table_name = 'account';
+--     SELECT COUNT(*) INTO v_audit_count_after FROM audit_log WHERE table_name = 'account';
     
-    IF v_audit_count_after > v_audit_count_before THEN
-        RAISE NOTICE 'تغییر موجودی ثبت شد';
-    ELSE
-        RAISE EXCEPTION 'تست موجودی شکست خورد';
-    END IF;
-END $$;
+--     IF v_audit_count_after > v_audit_count_before THEN
+--         RAISE NOTICE 'تغییر موجودی ثبت شد';
+--     ELSE
+--         RAISE EXCEPTION 'تست موجودی شکست خورد';
+--     END IF;
+-- END $$;
 
 -- درج تراکنش جدید
-DO $$
-BEGIN
-    INSERT INTO transactions (
-        transaction_type, amount, description, 
-        source_account, destination_account, status
-    )
-    VALUES (
-        'TRANSFER', 50000, 'تست Audit System',
-        '1001000100010001', '2002000200020001', 'COMPLETED'
-    );
+-- DO $$
+-- BEGIN
+--     INSERT INTO transactions (
+--         transaction_type, amount, description, 
+--         source_account, destination_account, status
+--     )
+--     VALUES (
+--         'TRANSFER', 50000, 'تست Audit System',
+--         '1001000100010001', '2002000200020001', 'COMPLETED'
+--     );
     
-    IF EXISTS (
-        SELECT 1 FROM audit_log 
-        WHERE table_name = 'transactions' 
-        AND operation_type = 'INSERT'
-        AND changed_at >= CURRENT_TIMESTAMP - INTERVAL '5 seconds'
-    ) THEN
-        RAISE NOTICE 'تراکنش ثبت شد';
-    ELSE
-        RAISE EXCEPTION 'تست تراکنش شکست خورد';
-    END IF;
-END $$;
+--     IF EXISTS (
+--         SELECT 1 FROM audit_log 
+--         WHERE table_name = 'transactions' 
+--         AND operation_type = 'INSERT'
+--         AND changed_at >= CURRENT_TIMESTAMP - INTERVAL '5 seconds'
+--     ) THEN
+--         RAISE NOTICE 'تراکنش ثبت شد';
+--     ELSE
+--         RAISE EXCEPTION 'تست تراکنش شکست خورد';
+--     END IF;
+-- END $$;
+
+---------------------------------------------------------------------------------------------------
