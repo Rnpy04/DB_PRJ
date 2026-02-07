2f91c15fda55d5a93bd191b1fbb6590e4e2f008e
Rnpy04 <arenyusefi@gmail.com>
2026-02-07 07:44:04 +0330

    Comment unnecessary lines

--- prj2/phase8-roles.sql
+++ prj2/phase8-roles.sql
@@ -20,10 +20,8 @@ GRANT SELECT ON ALL TABLES IN SCHEMA public TO role_bank_admin;
 -- اعطای دسترسی‌های آینده (برای جداول جدید)
 ALTER DEFAULT PRIVILEGES IN SCHEMA public 
     GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO role_bank_admin;
-
 ALTER DEFAULT PRIVILEGES IN SCHEMA public 
     GRANT USAGE, SELECT ON SEQUENCES TO role_bank_admin;
-
 ALTER DEFAULT PRIVILEGES IN SCHEMA public
 GRANT EXECUTE ON FUNCTIONS TO role_bank_admin;
 
@@ -47,11 +45,6 @@ GRANT UPDATE (status, account_type) ON account TO role_branch_employee;
 GRANT UPDATE (phone, address,email) ON customer TO role_branch_employee;
 GRANT UPDATE (status) ON card TO role_branch_employee;
 
-GRANT USAGE, SELECT ON SEQUENCE customer_customer_id_seq TO role_branch_employee;
-GRANT USAGE, SELECT ON SEQUENCE transactions_transaction_id_seq TO role_branch_employee;
-GRANT USAGE, SELECT ON SEQUENCE payment_payment_id_seq TO role_branch_employee;
-GRANT USAGE, SELECT ON SEQUENCE loan_loan_id_seq TO role_branch_employee;
-
 -- دسترسی به Views (برای گزارش‌گیری)
 GRANT SELECT ON vw_customer_accounts_summary TO role_branch_employee;
 GRANT SELECT ON vw_active_loans TO role_branch_employee;
@@ -64,44 +57,49 @@ GRANT EXECUTE ON PROCEDURE pr_pay_bill TO role_branch_employee;
 GRANT EXECUTE ON PROCEDURE pr_register_loan TO role_branch_employee;
 
 
+GRANT USAGE, SELECT ON SEQUENCE customer_customer_id_seq TO role_branch_employee;
+GRANT USAGE, SELECT ON SEQUENCE transactions_transaction_id_seq TO role_branch_employee;
+GRANT USAGE, SELECT ON SEQUENCE payment_payment_id_seq TO role_branch_employee;
+GRANT USAGE, SELECT ON SEQUENCE loan_loan_id_seq TO role_branch_employee;
+
 --------------------------------------------------------------------------------------------------
 
-CREATE USER admin_user WITH PASSWORD 'Admin@20!Secure';
-GRANT role_bank_admin TO admin_user;
+-- CREATE USER admin_user WITH PASSWORD 'Admin@20!Secure';
+-- GRANT role_bank_admin TO admin_user;
 
-CREATE USER employee_user1 WITH PASSWORD 'Employee1@20!';
-GRANT role_branch_employee TO employee_user1;
+-- CREATE USER employee_user1 WITH PASSWORD 'Employee1@20!';
+-- GRANT role_branch_employee TO employee_user1;
 
-CREATE USER employee_user2 WITH PASSWORD 'Employee2@20!';
-GRANT role_branch_employee TO employee_user2;
+-- CREATE USER employee_user2 WITH PASSWORD 'Employee2@20!';
+-- GRANT role_branch_employee TO employee_user2;
 
 --------------------------------------------
 
-SELECT 
-    'role_bank_admin' AS role_name,
-    table_name,
-    string_agg(privilege_type, ', ') AS privileges
-FROM information_schema.role_table_grants
-WHERE grantee = 'role_bank_admin'
-  AND table_schema = 'public'
-GROUP BY table_name
-ORDER BY table_name;
+-- SELECT 
+--     'role_bank_admin' AS role_name,
+--     table_name,
+--     string_agg(privilege_type, ', ') AS privileges
+-- FROM information_schema.role_table_grants
+-- WHERE grantee = 'role_bank_admin'
+--   AND table_schema = 'public'
+-- GROUP BY table_name
+-- ORDER BY table_name;
 
-SELECT 
-    'role_branch_employee' AS role_name,
-    table_name,
-    string_agg(privilege_type, ', ') AS privileges
-FROM information_schema.role_table_grants
-WHERE grantee = 'role_branch_employee'
-  AND table_schema = 'public'
-GROUP BY table_name
-ORDER BY table_name;
+-- SELECT 
+--     'role_branch_employee' AS role_name,
+--     table_name,
+--     string_agg(privilege_type, ', ') AS privileges
+-- FROM information_schema.role_table_grants
+-- WHERE grantee = 'role_branch_employee'
+--   AND table_schema = 'public'
+-- GROUP BY table_name
+-- ORDER BY table_name;
 
-SELECT 
-    r.rolname AS role,
-    m.rolname AS member
-FROM pg_roles r
-LEFT JOIN pg_auth_members am ON r.oid = am.roleid
-LEFT JOIN pg_roles m ON am.member = m.oid
-WHERE r.rolname LIKE 'role_%' OR r.rolname LIKE '%_user%'
-ORDER BY r.rolname, m.rolname;
+-- SELECT 
+--     r.rolname AS role,
+--     m.rolname AS member
+-- FROM pg_roles r
+-- LEFT JOIN pg_auth_members am ON r.oid = am.roleid
+-- LEFT JOIN pg_roles m ON am.member = m.oid
+-- WHERE r.rolname LIKE 'role_%' OR r.rolname LIKE '%_user%'
+-- ORDER BY r.rolname, m.rolname;
